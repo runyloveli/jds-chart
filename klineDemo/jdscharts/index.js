f0825eeb806eb065a332705b58b7681562340bee
king <jianguo.ning@yintech.cn>
2017-11-03 09:09:32 +0800

    变目录

--- klineDemo/jdscharts/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by JDR on 2017/8/24.
- */
-module.exports = require("./src/jdscharts.js");
-//require('./src/component/title');
-require('./src/chart/line');
-require('./src/component/grid');
--- klineDemo/start.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by JDR on 2017/8/24.
- */
-module.exports = require("./src/jdschart");
-require('./src/css/global.css');
-//require('./src/chart/line');
-//require('./src/component/grid');
